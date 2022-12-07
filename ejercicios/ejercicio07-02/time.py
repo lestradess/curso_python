88b8e3ccf6d917ed7a104b9e341267ebe2c92cb4
lestrad <lestrades@gmail.com>
2022-12-07 11:28:59 +0100

    ejercicio7-2

--- ejercicios/ejercicio07-02/time.py
+++ ejercicios/ejercicio07-02/time.py
@@ -1,5 +1,16 @@
 import time
 
+h= int(time.strftime('%H'))
+m= int(time.strftime("%M"))
 
+def trabajo():
+    hr = 19-h
+    mt = 59-m
+    return "Te quedan {} horas y {} minutos de curro.".format (hr,mt) 
 
-print(time.localtime())
+if (h >19):
+    print("Ya devieras estar fuera del trabajo")
+else:
+    print("Son las: {}:{}".format(h,m))
+    print(trabajo())
+
