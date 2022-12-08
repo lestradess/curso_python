5e64eae31b318ddc54239bc3d4da70ba88794dac
lestrad <lestrades@gmail.com>
2022-12-08 12:44:37 +0100

    ejercicio10-01

--- ejercicios/ejercico10-01/main.py
+++ ejercicios/ejercico10-01/main.py
@@ -4,8 +4,10 @@ root = Tk()
 root.geometry("150x150")
 root.title("Elegir opción")
 
+
 def seleccion():
     label.config(text="Eres {}".format(opcion.get()))
+
 
 def reset():
     opcion.set("None")
@@ -15,9 +17,12 @@ def reset():
 opcion = StringVar()
 opcion.set("None")
 
-Radiobutton(root, text="Hola", var=opcion,value=" Español", command=seleccion).pack()
-Radiobutton(root, text="Hello", var=opcion,value=" Inglés", command=seleccion).pack()
-Radiobutton(root, text="Hallo", var=opcion,value=" Alemán", command=seleccion).pack()
+Radiobutton(root, text="Hola", var=opcion,
+            value=" Español", command=seleccion).pack()
+Radiobutton(root, text="Hello", var=opcion,
+            value=" Inglés", command=seleccion).pack()
+Radiobutton(root, text="Hallo", var=opcion,
+            value=" Alemán", command=seleccion).pack()
 
 label = Label(root)
 label.pack()
@@ -25,4 +30,3 @@ label.pack()
 Button(root, text="Reiniciar", command=reset).pack()
 
 root.mainloop()
-
